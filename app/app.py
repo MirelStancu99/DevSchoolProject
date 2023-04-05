01654362218b0e670d109988a442a806cc8c6fb7
Mirel Stancu <stancumirel26@gmai.com>
2023-04-05 22:33:30 +0300

    refactoring app

--- app/app.py
+++ app/app.py
@@ -1,9 +1,9 @@
 # Importăm bibliotecile necesare
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import socket
 import sqlite3
 import os
-
+import requests
 # Inițializăm aplicația Flask
 app = Flask(__name__)
 
@@ -255,7 +255,74 @@ def contact():
     conn.close()
 
     return render_template('contact.html', HOSTNAME = hostname, IP = ip, MAXCALORIES=maxResult,MINCALORIES=minResult, TOTALALIMENTE=total_foods)
+@app.route('/liveness')
+def liveness():
+    #MAIN BARS
+    hostname,ip = fetchAddressDetail()
 
+    conn = get_db_connection()
+    maxCalories = conn.execute('SELECT denumire FROM foods ORDER BY calorii DESC LIMIT 1').fetchone()
+    minCalories = conn.execute('SELECT denumire FROM foods ORDER BY calorii ASC LIMIT 1').fetchone()
+    resultFoodsCounter = conn.execute('SELECT COUNT(*) FROM foods').fetchone()
+    total_foods = str(resultFoodsCounter[0])
+    maxResult = maxCalories["denumire"]
+    minResult = minCalories["denumire"]
+    
+    conn.close()
+
+    api_url = "https://jsonplaceholder.typicode.com/users"
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        responseSTR = "API is alive"
+        color = "#c8e6c9"
+    else:
+        responseSTR = "API is down"
+        color = "red"
+    return render_template('liveness.html',COLOR = color,RESPONSE = responseSTR,HOSTNAME = hostname, IP = ip, MAXCALORIES=maxResult,MINCALORIES=minResult, TOTALALIMENTE=total_foods)
+@app.route('/api')
+def api():
+    #MAIN BARS
+    hostname,ip = fetchAddressDetail()
+
+    conn = get_db_connection()
+    maxCalories = conn.execute('SELECT denumire FROM foods ORDER BY calorii DESC LIMIT 1').fetchone()
+    minCalories = conn.execute('SELECT denumire FROM foods ORDER BY calorii ASC LIMIT 1').fetchone()
+    resultFoodsCounter = conn.execute('SELECT COUNT(*) FROM foods').fetchone()
+    total_foods = str(resultFoodsCounter[0])
+    maxResult = maxCalories["denumire"]
+    minResult = minCalories["denumire"]
+    
+    conn.close()
+
+    api_url = "https://jsonplaceholder.typicode.com/users"
+    response = requests.get(api_url)
+    responses = response.json()  # Parsați JSON-ul într-un obiect Python
+    
+    return render_template('api.html',responses = responses,HOSTNAME = hostname, IP = ip, MAXCALORIES=maxResult,MINCALORIES=minResult, TOTALALIMENTE=total_foods)
+
+@app.route('/apiOrdonat')
+def apiOrdonat():
+    #MAIN BARS
+    hostname,ip = fetchAddressDetail()
+
+    conn = get_db_connection()
+    maxCalories = conn.execute('SELECT denumire FROM foods ORDER BY calorii DESC LIMIT 1').fetchone()
+    minCalories = conn.execute('SELECT denumire FROM foods ORDER BY calorii ASC LIMIT 1').fetchone()
+    resultFoodsCounter = conn.execute('SELECT COUNT(*) FROM foods').fetchone()
+    total_foods = str(resultFoodsCounter[0])
+    maxResult = maxCalories["denumire"]
+    minResult = minCalories["denumire"]
+    
+    conn.close()
+
+    # with open('users.json') as f:
+    # responses = json.load(f)
+    api_url = "https://jsonplaceholder.typicode.com/users"
+    response = requests.get(api_url)
+    responses = response.json()  # Parsați JSON-ul într-un obiect Python
+    responses = sorted(responses, key=lambda k: k['name'])
+    
+    return render_template('apiOrdonat.html',responses = responses,HOSTNAME = hostname, IP = ip, MAXCALORIES=maxResult,MINCALORIES=minResult, TOTALALIMENTE=total_foods)
 # Rulăm aplicația pe localhost, portul 5000
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
