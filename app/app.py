49175eea0127115ec6cb8fabe79fb524bc8a9b7f
Mirel Stancu <stancumirel26@gmai.com>
2023-04-10 15:30:24 +0300

    FINAL

--- app/app.py
+++ app/app.py
@@ -255,10 +255,9 @@ def contact():
     conn.close()
 
     return render_template('contact.html', HOSTNAME = hostname, IP = ip, MAXCALORIES=maxResult,MINCALORIES=minResult, TOTALALIMENTE=total_foods)
+
 @app.route('/prezentare')
 def prezentare():
-    
-
     return render_template('prezentare.html')
 
 @app.route('/liveness')
